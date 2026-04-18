5eb03d76e4282a200557226b894576b9dc9b61bf
Suriya Narayanan <suriyanarayanan@gmail.com>
2026-04-18 23:32:14 +0530

    Update main.dart UI

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text("My App"),),
-        body: Center(child: Text("Hello cse"),),
+        body: Center(child: Text("Hello mapla"),),
       ),
     );
   }
